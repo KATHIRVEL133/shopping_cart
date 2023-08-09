0da406d6c1fbe4f882b96cabcc9ce8aed1db2bc5
KATHIRVEL133 <100035796+KATHIRVEL133@users.noreply.github.com>
2023-08-09 23:16:10 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,80 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.webp";
+import product7 from "./assets/products/7.webp";
+import product8 from "./assets/products/8.webp";
+import product9 from "./assets/products/9.s20ultra.webp";
+import product10 from "./assets/products/10.gp.jpg";
+import product11 from "./assets/products/11.dell.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone 13 pro max",
+    price: 134999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Samsung S23 Ultra",
+    price: 124999.0,
+    productImage: product9,
+  },
+  {
+    id: 3,
+    productName: "Google Pixel 7 Pro",
+    price: 79400.0,
+    productImage: product10,
+  },
+  {
+    id: 4,
+    productName: "Macbook Pro 2022 (M1)",
+    price: 178999.0,
+    productImage: product2,
+  },
+  {
+    id: 5,
+    productName: "Dell XPS 13 9370 Intel Core i5",
+    price: 178999.0,
+    productImage: product11,
+  },
+  {
+    id: 6,
+    productName: "Canon EOS 350D Digital SLR Camera",
+    price: 28999.0,
+    productImage: product3,
+  },
+  {
+    id: 7,
+    productName: "Denim Jacket",
+    price: 2228.0,
+    productImage: product4,
+  },
+  {
+    id: 8,
+    productName: "SHIRT by GOLF WANG",
+    price: 9120.0,
+    productImage: product7,
+  },
+  {
+    id: 9,
+    productName: "SPECTRUM LS TEE",
+    price: 2068.0,
+    productImage: product6,
+  },
+  {
+    id: 10,
+    productName: "LED Light Strips",
+    price: 3299.99,
+    productImage: product5,
+  },
+  {
+    id: 11,
+    productName: "DON'T TRIP UNSTRUCTURED HAT",
+    price: 1540.0,
+    productImage: product8,
+  },
+];
